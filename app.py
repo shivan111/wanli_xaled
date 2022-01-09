8efdc466aac6a7fd34a01643e3215addd187e452
shivan111 <43236931+shivan111@users.noreply.github.com>
2022-01-09 19:54:09 +0100

    09.01.22 after removing extra files

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, url_for
+import jinja2
+
+
+# var part ####################################
+var_top_nav_bar=['Главная',
+                 'Полезная информация',
+                 'Терминология',
+                 'Оптовая торговля фруктами и овощами',
+                 'kontakt']
+
+var_language={1:'Rus',
+              2:'Eng',
+              3:'Arab'}
+
+company_title= {'ru':'Продимпэкс',
+                'en':'Prodimaks'}
+
+
+
+# Flask Part ####################################
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET']) 
+def index(): 
+    global company_title_ru
+    page_title='Index'
+    return render_template('index.html',
+                           page_title=page_title,company_title=company_title,var_top_nav_bar=var_top_nav_bar,
+                           var_language=var_language)
+
+
+@app.route('/Полезная информация')
+def useful_info():
+    page_title='Kontakt'
+    return render_template('useful_info.html',
+                           page_title=page_title,var_top_nav_bar=var_top_nav_bar,
+                           var_language=var_language)
+
+@app.route('/Терминология')
+def term():
+    page_title='Терминология'
+    return render_template('term.html',
+                           page_title=page_title,var_top_nav_bar=var_top_nav_bar,
+                           var_language=var_language)
+
+
+@app.route('/kontakt')
+def kontakt():
+    page_title='Kontakt'
+    return render_template('kontakt.html',
+                           page_title=page_title,var_top_nav_bar=var_top_nav_bar,
+                           var_language=var_language)
+
+
+@app.route('/Оптовая_торговля_фруктами_овощами',)
+def obt_fru_obst():
+    page_title='obt_fru_obst'
+    return render_template('obt_fru_obst.html',
+                           page_title=page_title,var_top_nav_bar=var_top_nav_bar,
+                           var_language=var_language)
+
+@app.route('/about')
+def about():
+    page_title='About'
+    return render_template('about.html',
+                           page_title=page_title,var_top_nav_bar=var_top_nav_bar,
+                           var_language=var_language)
+
+################################################
+#...
+if __name__ == '__main__':
+    app.run(debug=True )
+    
---
+++ static/js/main.js
@@ -0,0 +1,85 @@
+/* 
+ _____   _           _         _                        _                  
+|_   _| | |         | |       | |                      | |                 
+  | |   | |__   __ _| |_ ___  | |_ ___  _ __ ___   __ _| |_ ___   ___  ___ 
+  | |   | '_ \ / _` | __/ _ \ | __/ _ \| '_ ` _ \ / _` | __/ _ \ / _ \/ __|
+ _| |_  | | | | (_| | ||  __/ | || (_) | | | | | | (_| | || (_) |  __/\__ \
+ \___/  |_| |_|\__,_|\__\___|  \__\___/|_| |_| |_|\__,_|\__\___/ \___||___/
+
+Oh nice, welcome to the js file of dreams.
+Enjoy responsibly!
+@ihatetomatoes
+
+*/
+/* start  lo.html.js */
+
+$(document).ready(function() {
+
+    setTimeout(function() {
+        $('body').addClass('loaded');
+        $('h1').css('color', '#222222');
+    }, 3000);
+
+});
+/* end lo.html.js */
+
+/**################################################################ */
+/* print something in console ###################### */
+var text = "i was herie saab"
+console.log(text)
+
+/**################################################################ */
+/* start js obt_fru_obst.html */
+
+
+filterSelection("all") // Execute the function and show all columns
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("column");
+    if (c == "all") c = "";
+    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
+
+/* end js obt_fru_obst.html */
+/*#########################################################################*/
