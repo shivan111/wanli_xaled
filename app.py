e49671a7697342cb1d3f2bac505c76d4825b88a5
shivan111 <43236931+shivan111@users.noreply.github.com>
2022-01-10 10:43:07 +0100

    del agyrzi

--- app.py
+++ app.py
@@ -72,5 +72,6 @@ def about():
 ################################################
 #...
 if __name__ == '__main__':
-    app.run(debug=True )
+    #app.run(debug=True ) # $$$ im heroku i should turn of the debug
+    app.run()
     
